a34509c0bf9f321804e37f5b0c731805ece46e63
Kleissner <peter@kleissner.investments>
2021-12-07 21:43:47 +0100

    New command: get block

--- Command Line.go
+++ Command Line.go
@@ -44,6 +44,7 @@ func showHelp(output io.Writer) {
 		"warehouse store               Store data into local warehouse\n"+
 		"dht get                       Get data via DHT by hash\n"+
 		"dht store                     Store data into DHT\n"+
+		"get block                     Get block from remote peer\n"+
 		"log error                     Set error log output\n"+
 		"\n")
 }
@@ -400,6 +401,45 @@ func userCommands(input io.Reader, output io.Writer, terminateSignal chan struct
 			}
 
 			go transferCompareFile(peer, fileHash)
+
+		case "get block":
+			fmt.Fprintf(output, "Enter peer ID or node ID:\n")
+			nodeIDA, _, terminate := getUserOptionString(reader, terminateSignal)
+			if terminate {
+				return
+			}
+			fmt.Fprintf(output, "Enter block number:\n")
+			blockNumber, _, terminate := getUserOptionInt(reader, terminateSignal)
+			if terminate {
+				return
+			}
+
+			nodeID, valid2 := webapi.DecodeBlake3Hash(nodeIDA)
+			publicKey, err3 := core.PublicKeyFromPeerID(nodeIDA)
+
+			if !valid2 && err3 != nil {
+				fmt.Fprintf(output, "Invalid peer ID or node ID.\n")
+				break
+			} else if blockNumber < 0 {
+				fmt.Fprintf(output, "Invalid block number.\n")
+			}
+
+			var peer *core.PeerInfo
+			var err error
+			timeout := time.Second * 10
+
+			if valid2 {
+				peer, err = webapi.PeerConnectNode(nodeID, timeout)
+			} else if err3 == nil {
+				peer, err = webapi.PeerConnectPublicKey(publicKey, timeout)
+			}
+			if err != nil {
+				fmt.Fprintf(output, "Could not connect to peer: %s\n", err.Error())
+				break
+			}
+
+			go blockTransfer(peer, uint64(blockNumber))
+
 		}
 	}
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/PeernetOfficial/Cmd
 go 1.16
 
 require (
-	github.com/PeernetOfficial/core v0.0.0-20211130025319-68d8c1187273
+	github.com/PeernetOfficial/core v0.0.0-20211204025234-34e48c09df12
 	github.com/google/uuid v1.3.0
 	github.com/gorilla/websocket v1.4.3-0.20210424162022-e8629af678b7
 )
